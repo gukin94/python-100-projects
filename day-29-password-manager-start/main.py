efe2da01bcaacad0249f2de34c8ba650fb3a1f13
Gukin Han <62578503+gukin94@users.noreply.github.com>
2022-06-27 13:11:40 +0900

    change

--- day-29-password-manager-start/main.py
+++ day-29-password-manager-start/main.py
@@ -1,5 +1,16 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+
+canvans = Canvas()
+
+img = PhotoImage("logo.png")
+
+
+window.mainloop()
